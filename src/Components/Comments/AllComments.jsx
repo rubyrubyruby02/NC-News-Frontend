4de258e6a16dc61c1e95bee58c9e6d712ce9de3b
Ruby <ruby.porter@tiscali.co.uk>
2023-09-05 14:16:45 +0100

    adds isLoading and noComments for article with no comments

--- src/Components/Comments/AllComments.jsx
+++ src/Components/Comments/AllComments.jsx
@@ -1,4 +1,5 @@
 import Header from "../Header"
+import Loading from "../ErrorAndLoading/Loading";
 import {useState, useEffect} from 'react'
 import {getCommentsOnArticle} from "../../../Utils/apiRequests.js"
 import { useParams } from "react-router-dom";
@@ -7,21 +8,36 @@ import CommentCard from "./CommentCard";
 const AllComments = () => {
     
     const { article_id } = useParams();
+
     const [comments, setComments] = useState([])
+    const [isLoading, setisLoading] = useState(false)
+    const [noComments, setNoComments]=useState(false)
 
     useEffect(()=> {
+        setisLoading(true)
+
         getCommentsOnArticle(article_id)
         .then(({comments})=> {
-            setComments(comments)
+
+            if(comments.length === 0){
+                setisLoading(false)
+                setNoComments(true)
+            }
+            else {
+                setComments(comments)
+                setisLoading(false)
+            }
         })
     }, [])
+
+    if(isLoading) return <Loading />
 
     return (
         <>
         <Header />
         <h3>Article Number: {article_id}</h3>
-        <section>
-            {comments.map((comment)=> {
+        <section> 
+            {noComments ? (<h3>No comments yet</h3>) : comments.map((comment)=> {
                 return (
                 <>
                 <CommentCard key={comment.comment_id} comment={comment} />
